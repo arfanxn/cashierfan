3069ce7ebcfee799fcf3600b791b2bb0a0b381c4
mysql73 <arfan2173@gmail.com>
2022-06-05 02:57:17 +0700

    create a service file/class  for handling verificationcode

--- app/Services/VerificationCodeService.php
+++ app/Services/VerificationCodeService.php
@@ -2,10 +2,15 @@
 
 namespace App\Services;
 
+use App\Helpers\Redirect;
 use App\Helpers\Str;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\RequiredIf;
 
 class VerificationCodeService
 {
@@ -58,16 +63,97 @@ class VerificationCodeService
         $isCodeMatch = $verificationCode === $code;
 
         if ($onErrorCustomResponse === false && $isCodeMatch === false) {
-            response()->json([
-                "errors" => [
-                    "verification_code" => "Verification Code is incorrect",
-                ]
-            ])->throwResponse();
-            exit();
+            $errors = [
+                "verification_code" => ["Verification Code is incorrect or expired"],
+            ];
+
+            request()->expectsJson() ?
+                response()->json(["errors" => $errors])->throwResponse()
+                : Redirect::now(redirect()->back()->withErrors($errors));
+
+            return exit();
         }
 
         if ($isCodeMatch) $query->delete();
 
         return $isCodeMatch;
+    }
+
+
+
+
+
+
+
+
+
+    //  static validation methods
+    public static function valiateVerifiable(Request $request)
+    {
+        $validator  = Validator::make($request->only(['email', "phone_number", "verifiable"]), [
+            "email" => [
+                new RequiredIf(function () use ($request) {
+                    return !boolval(/**/$request->verifiable ?? $request->phone_number/**/);
+                }), "email"
+            ],
+            "phone_number" => [
+                new RequiredIf(function () use ($request) {
+                    return !boolval(/**/$request->verifiable ?? $request->email/**/);
+                }),
+            ],
+            "verifiable" => [new RequiredIf(function () use ($request) {
+                return !boolval(/**/$request->email ?? $request->phone_number/**/);
+            }), "string"],
+        ]);
+
+        if ($validator->fails()) {
+            if ($request->expectsJson()) {
+                response()->json(["errors" => $validator->errors()]/**/)->throwResponse();
+            } else {
+                Redirect::now(
+                    redirect()->back()->withErrors($validator->errors())
+                );
+            }
+            return exit();
+        }
+
+        $verifiable = $request->verifiable ?? $request->email ?? $request->phone_number;
+
+        return $verifiable;
+    }
+
+    public static function validateCode(Request $request)
+    {
+        $validator = Validator::make(
+            $request->only(['verification_code', "code"]),
+            [
+                'verification_code' =>   [
+                    new RequiredIf(function () use ($request) {
+                        return !boolval(/**/$request->code/**/);
+                    }), "numeric", "digits:6"
+                ],
+                "code" => [
+                    new RequiredIf(function () use ($request) {
+                        return !boolval(/**/$request->verification_code/**/);
+                    }),
+                    "numeric", "digits:6"
+                ],
+            ]
+        );
+
+        if ($validator->fails()) {
+            if ($request->expectsJson()) {
+                response()->json(["errors" => $validator->errors()]/**/)->throwResponse();
+            } else {
+                Redirect::now(
+                    redirect()->back()->withErrors($validator->errors())
+                );
+            }
+            return exit();
+        }
+
+        $code = $validator->validated()['verification_code'] ?? $validator->validated()['code'];
+
+        return $code;
     }
 }
