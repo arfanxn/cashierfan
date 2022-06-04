e26706a4323efff826159613fe3cb1890ebc928c
mysql73 <arfan2173@gmail.com>
2022-06-05 06:37:29 +0700

    create send vcode service

---
+++ resources/js/Services/AuthService.js
@@ -0,0 +1,12 @@
+import { fetchAsJSON } from "../Mixins/Fetch";
+
+export async function sendVerificationCode (verifiable) {
+
+    verifiable = typeof verifiable != "object" ? 
+        { "verifiable" : verifiable }
+        : verifiable  ;
+
+    return await fetchAsJSON(route("auth.vc.send"), {
+        body: verifiable
+    }) ;
+}
