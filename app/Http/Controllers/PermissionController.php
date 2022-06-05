3994b260f06d6619a0a857d44ca242a298194281
mysql73 <arfan2173@gmail.com>
2022-06-05 23:25:49 +0700

    add middlewares

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -8,6 +8,17 @@ use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(
+            'permission:permissions.index|permissions.create|permissions.edit|permissions.delete',
+            ['only' => ['index', 'store']]
+        );
+        $this->middleware('permission:permissions.create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:permissions.edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:permissions.delete', ['only' => ['destroy']]);
+    }
+
     public  function __invoke(Request $request)
     {
         $keyword = $request->query('keyword');
