dfe03c4d79f21223c13576db719b39359cfa5f90
mysql73 <arfan2173@gmail.com>
2022-06-07 08:15:38 +0700

    fixed get request bug

--- resources/js/Mixins/Fetch.js
+++ resources/js/Mixins/Fetch.js
@@ -5,17 +5,22 @@ const csrf_token = computed(() => usePage().props.value.csrf_token);
 export async function fetchAsJSON (url , {
     method, headers, body
 }) {
-    return await fetch(url, {
+    const data  = {
             "method": !method ? "POST" : method,
             "headers": {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json' , 
                 ...headers,
             },
-            body: JSON.stringify({
-                _token: csrf_token.value , ...body
-            }),
-        }).then(async response => ({
+        }; 
+
+    if (method !== "GET" && method !== "HEAD") {
+        data.body = JSON.stringify({
+            _token: csrf_token.value , ...body
+        })
+    }
+
+    return await fetch(url, data).then(async response => ({
             response : await response , json : await response.json()
         })) ;
 }
