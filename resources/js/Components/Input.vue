8d5712e0306fffb178f3d1a7c1ad3352fcbb675c
mysql73 <arfan2173@gmail.com>
2022-06-09 11:54:54 +0700

    when input type ===  number disable alpahbet

--- resources/js/Components/Input.vue
+++ resources/js/Components/Input.vue
@@ -1,12 +1,30 @@
 <template>
-    <input
+    <input :ref="(elem) => { if (elem) elements.input = elem }"
         class="w-full px-2 py-1 border border-gray-300 file:text-slate-600 focus:outline-none focus:outline-indigo-900/25 focus:outline-4 focus:outline-offset-0 file:mr-2 file:py-1 lg:file:px-4 file:px-2 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-100/50 hover:file:bg-indigo-300/50"
         type="text" @input="throwEmit">
 </template>
 
 <script setup>
-import { defineEmits, } from 'vue';
-const emit = defineEmits(['onInput'])
+import { defineEmits, onMounted, onUnmounted, reactive } from 'vue';
+import { tap } from '../Helpers';
+const emit = defineEmits(['onInput']);
+
+const elements = reactive({
+    input: null,
+})
+function onKeypress(event) {
+    const keyCode = event.keyCode;
+    if (keyCode < 48 || keyCode > 57) return event.preventDefault();
+}
+onMounted(() => {
+    if (elements.input.type.toLowerCase() === "number")
+        elements.input.addEventListener("keypress", onKeypress)
+
+})
+onUnmounted(() => {
+    if (elements.input.type.toLowerCase() === "number")
+        elements.input.removeEventListener("keypress", onKeypress)
+})
 
 
 function throwEmit(event) {
