40210fb03abef45dbe5aa658a13e493656b8c24e
mysql73 <arfan2173@gmail.com>
2022-06-15 11:03:54 +0700

    commit

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -1,19 +1,16 @@
 <template>
-
     <Head>
         <title>Test</title>
     </Head>
 
-    <main class="flex flex-col justify-center mx-96 space-y-4">
+    <main class="flex flex-col justify-center space-y-4 mx-96">
         <h1>{{ $page.props.auth }}</h1>
         <InputCurrency />
     </main>
 </template>
 
 <script setup>
-import { Head } from "@inertiajs/inertia-vue3";
-import { Inertia } from "@inertiajs/inertia";
-import { toIDR, toCurrency } from "../Helpers";
-import InputCurrency from "../Components/InputCurrency.vue";
-
+// import { Head } from "@inertiajs/inertia-vue3";
+// import { Inertia } from "@inertiajs/inertia";
+// import { toIDR, toCurrency } from "../Helpers";
 </script>
