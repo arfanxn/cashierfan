132ad02ccf1d8a77cf14fa15b3d41c73ce0c2372
mysql73 <arfan2173@gmail.com>
2022-06-16 15:06:33 +0700

    formating

--- resources/js/Helpers.js
+++ resources/js/Helpers.js
@@ -1,103 +1,109 @@
-export function tap (variable , callback = null) {
+export function tap(variable, callback = null) {
     if (typeof variable == 'function' && callback == null) {
         return variable();
-    } else if (typeof variable != 'function' && typeof callback != 'function')  {
+    } else if (typeof variable != 'function' && typeof callback != 'function') {
         throw new Error('callback must be a function');
-    }    
-
-    return callback(variable) ;
-}
-
-export function randInt(min = 1 ,max = 9999999) {
-     return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-export function randChars(length = 16 ,chars ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') {
-
-    let randChars = `` ;
-
-    for ( var i = 0; i < length; i++ ) {
-        randChars  += chars.charAt(Math.floor(Math.random() * 
-        chars.length));
     }
 
-    return randChars  ;
+    return callback(variable);
 }
 
+export function randInt(min = 1, max = 9999999) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-export function imgValidation(filenameOrFilepath){
+export function randChars(
+    length = 16,
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+) {
+    let randChars = ``;
+
+    for (var i = 0; i < length; i++) {
+        randChars += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return randChars;
+}
+
+export function imgValidation(filenameOrFilepath) {
     var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif|\.jfif)$/i;
-    if(!allowedExtensions.exec(filenameOrFilepath)){
-        console.warn('Please upload file having extensions .jpeg/.jpg/.png/.gif/.jfif only.');
+    if (!allowedExtensions.exec(filenameOrFilepath)) {
+        console.warn(
+            'Please upload file having extensions .jpeg/.jpg/.png/.gif/.jfif only.'
+        );
         return false;
     }
-    return true ; 
+    return true;
 }
 
-export function isset(accessor , fallback = null , report = false){
+export function isset(accessor, fallback = null, report = false) {
     try {
-        const accessed = accessor() ;
-        if (typeof accessed ===  "undefined") throw new Error("Accessor returning undefined") ;
+        const accessed = accessor();
+        if (typeof accessed === 'undefined')
+            throw new Error('Accessor returning undefined');
 
-        return accessed ;
+        return accessed;
     } catch (err) {
         if (report === true) console.error(err);
 
-        if (typeof fallback === 'function')  return fallback(); 
+        if (typeof fallback === 'function') return fallback();
 
-        return fallback ? fallback : null; 
+        return fallback ? fallback : null;
     }
 }
 
 export function setURIQueryString(uri, key, value) {
-    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
-    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    var re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+    var separator = uri.indexOf('?') !== -1 ? '&' : '?';
     if (uri.match(re)) {
-        return uri.replace(re, '$1' + key + "=" + value + '$2');
-    }
-    else {
-        return uri + separator + key + "=" + value;
+        return uri.replace(re, '$1' + key + '=' + value + '$2');
+    } else {
+        return uri + separator + key + '=' + value;
     }
 }
 
-
-export function toCurrency (input, locale = "id-ID", currency = "IDR" ) {
-
+export function toCurrency(input, locale = 'id-ID', currency = 'IDR') {
     let fmt = String(input)
-        .replace(/(?<=\d)[.,](?!\d+$)/g, "")
-        .replace(",", ".");
-    const pts = fmt.split(".");
+        .replace(/(?<=\d)[.,](?!\d+$)/g, '')
+        .replace(',', '.');
+    const pts = fmt.split('.');
     if (pts.length > 1) {
-        if (+pts[0] === 0) fmt = pts.join(".");
-        else if (pts[1].length === 3) fmt = pts.join("");
+        if (+pts[0] === 0) fmt = pts.join('.');
+        else if (pts[1].length === 3) fmt = pts.join('');
     }
-    
+
     // format the input string to different formats
     const number = Number(fmt);
     const string = number.toFixed(2);
-    // detect if the number is valid  
+    // detect if the number is valid
     const isValid = isFinite(number);
 
     // regex pattern
-    const exceptNumericNCommaNDot = /[^0-9,.]/ig;
+    const exceptNumericNCommaNDot = /[^0-9,.]/gi;
 
     // format to currency
-    let output = new Intl.NumberFormat(locale, {style: 'currency', currency}).format(number);
+    let output = new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency
+    }).format(number);
 
     // get the currency symbol from "output" string
-    const symbol = String(output).match(exceptNumericNCommaNDot).join("").replace(/\s/ ,"");
+    const symbol = String(output)
+        .match(exceptNumericNCommaNDot)
+        .join('')
+        .replace(/\s/, '');
 
-    // remove the currency symbol  
-    const withoutSymbol = String(output).replace(exceptNumericNCommaNDot,'');
+    // remove the currency symbol
+    const withoutSymbol = String(output).replace(exceptNumericNCommaNDot, '');
 
     return {
         input,
         isValid,
         string,
         number,
-        currency, 
+        currency,
         output,
-        withoutSymbol , 
+        withoutSymbol,
         symbol
-    } ;
+    };
 }
